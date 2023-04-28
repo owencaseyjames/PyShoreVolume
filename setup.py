96725f8c73e957988474c3001df4f83a191cb555
owencaseyjames <103570277+owencaseyjames@users.noreply.github.com>
2023-04-28 13:12:06 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='PyShoreVolume',
+      version='1.0.0',
+      description='Pyhton Based Shoreline Change and Beach Volume Analysis Tool',
+      author='Owen Casey James',
+      author_email= 'owen.james@kcl.ac.uk',
+      licence='MIT',
+      long_description=open('README').read(),
+      install_requires= ,
+      url = ,
+      
+      packages=
+      )
+
+    
